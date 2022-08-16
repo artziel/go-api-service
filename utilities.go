9e7b6cbe9c1e65bc9a87ddaca07936b981e100cc
Artziel Narvaiza <artziel@gmail.com>
2022-08-15 22:40:41 -0500

    update

--- utilities.go
+++ utilities.go
@@ -10,14 +10,23 @@ import (
 	"strings"
 )
 
+/*
+Encode a golang error in JSON format and write to the response writer
+*/
 func RespondWithJSONError(w http.ResponseWriter, code int, err error) {
 	RespondWithJSON(w, code, map[string]string{"message": err.Error()})
 }
 
+/*
+Encode a text message in JSON format and write to the response writer
+*/
 func RespondWithJSONMessage(w http.ResponseWriter, code int, message string) {
 	RespondWithJSON(w, code, map[string]string{"message": message})
 }
 
+/*
+Encode a interface data in JSON format and write to the response writer
+*/
 func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
 
